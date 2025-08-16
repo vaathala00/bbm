2f79c434918554485abf7793aee107e349629f05
vaathala00 <165231338+vaathala00@users.noreply.github.com>
2025-08-16 11:41:47 +0530

    Files upload

--- scraper/index.js
+++ scraper/index.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const puppeteer = require("puppeteer");
 
 const urls = [
+  "https://bq32.short.gy/O7fkma",
   "https://www.twitch.tv/heyimenbhizeebal?sr=a",
   "https://www.twitch.tv/kukeeku",
   "https://bigbosslive.com/live/"
